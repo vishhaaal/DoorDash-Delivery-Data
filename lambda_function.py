e139ea0295bb980813f664b9529ce1eed27ff2a2
Vishal <negi2000vishal@gmail.com>
2024-03-18 16:46:50 +0530

    change

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,18 @@
-import pandas as pd
 import json
 import boto3
+import logging
 
 s3 = boto3.client('s3')
 sns = boto3.client('sns')
 target_bucket = 'doordash-target-zn-gds-de'
 sns_topic_arn = 'arn:aws:sns:ap-south-1:423736870603:assignmer-gds-de'
 
-def lambda_handler(event,context):
+def lambda_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    logger.info("Event: " + json.dumps(event))
+    
     try:
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = event['Records'][0]['s3']['object']['key']
@@ -19,17 +24,17 @@ def lambda_handler(event,context):
         target_key = key.split('/')[-1]
         target_key = 'filtered_'+ target_key
         target_path = f'{target_bucket}/{target_key}'
-        s3.put_object(Body = filtered_record_df.to_json(), Bucket = target_bucket, Key = target_key)
+        s3.put_object(Body=filtered_record_df.to_json(), Bucket=target_bucket, Key=target_key)
 
-        sns.publish(TopicArn = sns_topic_arn, Message = 'Processing Successful')
+        sns.publish(TopicArn=sns_topic_arn, Message='Processing Successful')
         return {
-            'statusCode' : 200,
-            'body' : json.dumps('Processing Successful')
+            'statusCode': 200,
+            'body': json.dumps('Processing Successful')
         }
     
     except Exception as e:
-        sns.publish(TopicArn = sns_topic_arn, Message = f'Processing Failed: {str(e)}')
+        sns.publish(TopicArn=sns_topic_arn, Message=f'Processing Failed: {str(e)}')
         return {
-            'statusCode' : 500,
-            'body' : json.dumps(f'Processing Failed: {str(e)}')
+            'statusCode': 500,
+            'body': json.dumps(f'Processing Failed: {str(e)}')
         }
